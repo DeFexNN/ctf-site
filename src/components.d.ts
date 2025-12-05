f7dd777fdec66bfa756bd7009baa29240eca4b6e
DeFexNN <super.denchuk@gmail.com>
2025-12-05 15:44:01 +0200

    add type declarations for components and enable allowJs in tsconfig

--- src/components.d.ts
+++ src/components.d.ts
@@ -1,6 +1,66 @@
-declare module "./components/Header";
-declare module "./components/Members";
-declare module "./components/CTFTable";
-declare module "./components/CTFCard";
-declare module "./components/Contacts";
-declare module "./components/Footer";
+import type { ComponentType } from "react";
+
+declare module "./components/Header" {
+	const Component: ComponentType<any>;
+	export default Component;
+}
+
+declare module "./components/Header.jsx" {
+	const Component: ComponentType<any>;
+	export default Component;
+}
+
+declare module "./components/Members" {
+	const Component: ComponentType<any>;
+	export default Component;
+}
+
+declare module "./components/Members.jsx" {
+	const Component: ComponentType<any>;
+	export default Component;
+}
+
+declare module "./components/CTFTable" {
+	const Component: ComponentType<any>;
+	export default Component;
+}
+
+declare module "./components/CTFTable.jsx" {
+	const Component: ComponentType<any>;
+	export default Component;
+}
+
+declare module "./components/CTFCard" {
+	const Component: ComponentType<any>;
+	export default Component;
+}
+
+declare module "./components/CTFCard.jsx" {
+	const Component: ComponentType<any>;
+	export default Component;
+}
+
+declare module "./components/Contacts" {
+	const Component: ComponentType<any>;
+	export default Component;
+}
+
+declare module "./components/Contacts.jsx" {
+	const Component: ComponentType<any>;
+	export default Component;
+}
+
+declare module "./components/Footer" {
+	const Component: ComponentType<any>;
+	export default Component;
+}
+
+declare module "./components/Footer.jsx" {
+    const Component: ComponentType<any>;
+    export default Component;
+}
+
+declare module "*.jsx" {
+    const Component: ComponentType<any>;
+    export default Component;
+}

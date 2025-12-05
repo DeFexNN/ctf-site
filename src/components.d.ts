3ff597112c236cbbbf76184e30338778b6f5b060
DeFexNN <super.denchuk@gmail.com>
2025-12-05 15:41:40 +0200

    add component declarations to typescript definition file

---
+++ src/components.d.ts
@@ -0,0 +1,6 @@
+declare module "./components/Header";
+declare module "./components/Members";
+declare module "./components/CTFTable";
+declare module "./components/CTFCard";
+declare module "./components/Contacts";
+declare module "./components/Footer";
